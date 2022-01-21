a8c229b8e935a3af6697aa9a5a0c725900c90d8f
Quanhu Sheng <shengqh@gmail.com>
2022-01-21 09:47:00 -0600

    update docker

--- docker/build_sqh.sh
+++ docker/build_sqh.sh
@@ -1,1 +1,3 @@
-docker build -t shengqh/cqs_bamsnap .
+date_string=`date +%Y%m%d`
+docker build -t shengqh/cqs_bamsnap:${date_string} .
+docker tag shengqh/cqs_bamsnap:${date_string} shengqh/cqs_bamsnap:latest
